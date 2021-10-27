ac04b5b825197eaafa9bc133c3db09fffde7bd5d
LiaoCheng <820564830@qq.com>
2021-10-27 09:12:34 +0800

    修改成有响应得形式！

--- t_DefRpc/t_DefRpc.h
+++ t_DefRpc/t_DefRpc.h
@@ -3,14 +3,15 @@
 using namespace std;
 
 
-int foo_4(int int1, const string &str1, float float1)
+string foo_4(int int1, const string &str1, float float1)
 {
     cout<< __func__<< int1<< str1<< float1<< endl;
-    return 4;
+    return "loc";
 }
-int Rpc(const string &sFunName, int int1, const string &str1, float float1)
+int Rpc(string& sResp, const string &sFunName, int int1, const string &str1, float float1)
 {
     cout<< __func__<< sFunName<< int1<< str1<< float1<< endl;
+    sResp = "rpc";
     return 100;
 }
 
@@ -18,21 +19,22 @@ int Rpc(const string &sFunName, int int1, const string &str1, float float1)
 
 #ifdef rpc
     //启用rpc
-    #define call(sFunName, ...) Rpc(#sFunName, __VA_ARGS__)
+    #define call(Resp, sFunName, ...) Rpc(Resp, #sFunName, __VA_ARGS__)
 #else
     //关闭rpc
-    #define call(sFunName, ...) sFunName(__VA_ARGS__)
+    #define call(Resp, sFunName, ...) 0; Resp = sFunName(__VA_ARGS__)
 #endif
 
 
 void testRpc()
 {
-    //书写版
-    int ret = call(foo_4, 10, "buttonrpc", 10.8);
-    cout<< __func__<< ret<< endl;
+    //以Rpc形式书写
+    string sResp;
+    int ret = call(sResp, foo_4, 10, "buttonrpc", 10.8);
+    cout<< __func__<< ret<< sResp<< endl;
 
-    //真正结果 1 no rpc; 2 use prc
-    //int foo4r =           foo_4  (10, "buttonrpc", 100, (float)10.8);
-    //int foo4r = call_fun("foo_4", 10, "buttonrpc", 100, (float)10.8));
+    //开关Rpc宏的选择结果
+    //int foo4r = 0;       sResp = foo_4  (10, "buttonrpc", 100, (float)10.8);  //本地调用
+    //int foo4r = call_fun(sResp, "foo_4", 10, "buttonrpc", 100, (float)10.8));  //rpc
 }
 
