881c386b506cb0cad78b9124ffa047eb8afe2a92
LiaoCheng <LiaoChengO@qq.com>
2021-10-27 10:36:14 +0800

    __VA_ARGS__前增加##，支持无参数

--- t_DefRpc/t_DefRpc.h
+++ t_DefRpc/t_DefRpc.h
@@ -19,10 +19,10 @@ int Rpc(string& sResp, const string &sFunName, int int1, const string &str1, flo
 
 #ifdef rpc
     //启用rpc
-    #define call(Resp, sFunName, ...) Rpc(Resp, #sFunName, __VA_ARGS__)
+    #define Call(Resp, FunName, ...) Rpc(Resp, #FunName, ##__VA_ARGS__)
 #else
     //关闭rpc
-    #define call(Resp, sFunName, ...) 0; Resp = sFunName(__VA_ARGS__)
+    #define Call(Resp, FunName, ...) 0; Resp = FunName(##__VA_ARGS__)
 #endif
 
 
@@ -30,11 +30,11 @@ void testRpc()
 {
     //以Rpc形式书写
     string sResp;
-    int ret = call(sResp, foo_4, 10, "buttonrpc", 10.8);
+    int ret = Call(sResp, foo_4, 10, "buttonrpc", 10.8);
     cout<< __func__<< ret<< sResp<< endl;
 
     //开关Rpc宏的选择结果
-    //int foo4r = 0;       sResp = foo_4  (10, "buttonrpc", 100, (float)10.8);  //本地调用
-    //int foo4r = call_fun(sResp, "foo_4", 10, "buttonrpc", 100, (float)10.8));  //rpc
+    //int ret = 0;  sResp = foo_4  (10, "buttonrpc", 100, 10.8);  //本地调用
+    //int ret = Rpc(sResp, "foo_4", 10, "buttonrpc", 100, 10.8));  //rpc
 }
 
